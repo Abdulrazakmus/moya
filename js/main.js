cdd801df27a01d283af278be8bc21c9c572784f5
abdulrazakmus <abdulrazak.jmus@gmail.com>
2022-08-21 13:12:27 +0300

    nime add kuza cave card

--- js/main.js
+++ js/main.js
@@ -145,3 +145,19 @@ $('.active-blog-slider').owlCarousel({
       });   
 
  });
+
+
+
+
+// Back to top button
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+  } else {
+      $('.back-to-top').fadeOut('slow');
+  }
+});
+$('.back-to-top').click(function () {
+  $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+  return false;
+});
